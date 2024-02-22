f1e7e22067685ef605f5214abb3cc376ad2da942
prasanshatamang <tamangprasansha291@gmail.com>
2024-02-22 13:00:29 +0545

    project setup done

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroImage from "../assests/heroImage.png";
+
+const Home = () => {
+  return (
+  <div>
+    
+    <img src={HeroImage} alt='' />
+  </div>
+  ); 
+};
+
+export default Home;   
